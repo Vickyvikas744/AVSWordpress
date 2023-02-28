acd300aa74f6e36bb762bff8381264371c147d43
vikas kumar <vikash102030@gamil.com>
2023-02-28 17:02:19 +0530

    initial commit

---
+++ wp-content/plugins/mega-addons-for-visual-composer/js/custommixitup.js
@@ -0,0 +1,1 @@
+jQuery(document).ready(function(){var sandwiches=document.querySelectorAll('.maw_portfolioGallery_container');sandwiches.forEach(function(sandwich,index){var mixer=mixitup(sandwich,{animation:{effects:"fadeIn",perspectiveDistance:"10px",perspectiveOrigin:"50% 50%",animateResizeContainer:false},controls:{scope:'local'}});});});
